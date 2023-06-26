caf7327a202551b095f9a3ca84fb421e33ff984c
adirizq <rizkyzadadi@gmail.com>
2023-06-26 22:19:43 +0700

    Initial commit

---
+++ app/src/main/java/com/pnj/makanyuk/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pnj.makanyuk.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pnj.makanyuk.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/pnj/makanyuk/fragment/TransactionHistoryFragment.kt
@@ -0,0 +1,28 @@
+package com.pnj.makanyuk.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pnj.makanyuk.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TransactionHistoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TransactionHistoryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_transaction_history, container, false)
+    }
+}
