ef2d7f1c3e6f854c91375ae9352dd626cf4d8456
Gilang <gilangadhiperkasa@gmail.com>
2023-06-28 09:06:58 +0700

    add show list menu

--- app/src/main/java/com/pnj/makanyuk/activity/MainActivity.kt
+++ app/src/main/java/com/pnj/makanyuk/activity/MainActivity.kt
@@ -2,11 +2,35 @@ package com.pnj.makanyuk.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.pnj.makanyuk.R
+import com.pnj.makanyuk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val splashScreen = installSplashScreen()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_view) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_menu, R.id.navigation_transaction_history, R.id.navigation_chat, R.id.navigation_profile
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
     }
 }
