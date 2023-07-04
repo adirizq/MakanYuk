0ef1d7b4c166015fdf7a359c321c0a3084d71c55
mhsalter <alter.neverend@gmail.com>
2023-07-04 10:35:24 +0700

    Login & Register

--- app/src/main/java/com/pnj/makanyuk/activity/LoginActivity.kt
+++ app/src/main/java/com/pnj/makanyuk/activity/LoginActivity.kt
@@ -3,13 +3,16 @@ package com.pnj.makanyuk.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.auth.FirebaseAuth
 import com.pnj.makanyuk.R
 import com.pnj.makanyuk.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private var firebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val splashScreen = installSplashScreen()
@@ -23,8 +26,35 @@ class LoginActivity : AppCompatActivity() {
         }
 
         binding.btnLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val email = binding.edtEmail.text.toString()
+            val password = binding.edtPassword.text.toString()
+            signin_firebase(email, password)
         }
     }
+
+    private fun signin_firebase(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        else {
+            Toast.makeText(this, "Lengkapi Input", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+//    override fun onStart() {
+//        super.onStart()
+//
+//        if (firebaseAuth.currentUser != null) {
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 }
--- app/src/main/java/com/pnj/makanyuk/fragment/ProfileFragment.kt
+++ app/src/main/java/com/pnj/makanyuk/fragment/ProfileFragment.kt
@@ -4,26 +4,61 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.pnj.makanyuk.R
+import com.pnj.makanyuk.databinding.FragmentProfileBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private val uid = "DNGowVPxTCy5T7bp5LrK"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnSave.setOnClickListener {
+            val new_password = binding.edtNewPassword.text.toString()
+            edit_password(new_password)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentProfileBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+        retrieveAllData(uid)
+
+        return binding.root
+    }
+
+
+
+    private fun retrieveAllData(uid: String) {
+
+    }
+
+    private fun edit_password(new_password: String) {
+        val user = FirebaseAuth.getInstance().currentUser
+        val new_password = new_password
+
+        user!!.updatePassword(new_password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(context, "Password Berhasil Diubah", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(context, "Password Gagal Diubah", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
